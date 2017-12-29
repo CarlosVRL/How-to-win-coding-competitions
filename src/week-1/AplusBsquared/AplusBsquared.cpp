7895cbf3fc2342ab586d964c43f49277a54f1575
CarlosVRL <carlos.varela.eng@gmail.com>
2017-12-29 01:54:31 -0600

    Corrected value overflow

--- src/week-1/AplusBsquared/AplusBsquared.cpp
+++ src/week-1/AplusBsquared/AplusBsquared.cpp
@@ -9,13 +9,11 @@ using namespace std;
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
-static const int EXPONENT_POWER = 2;
-
 int main(void)
 {
     int a, b;
     fin >> a >> b;
-    fout << a + pow(b, EXPONENT_POWER);
+    fout << a + (long long) (b) * b;
     return 0;
 }
 
