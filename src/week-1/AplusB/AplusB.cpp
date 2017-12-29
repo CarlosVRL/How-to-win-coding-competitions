72b67fd98c9e56b7fa3368078473d77cb8dd3ded
CarlosVRL <carlos.varela.eng@gmail.com>
2017-12-28 22:53:15 -0600

    Added simple Makefile and directory structure

---
+++ src/week-1/AplusB/AplusB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main(void) {
+    
+    cout << "Hello, worlds!\n";
+    
+    return 0;
+}
+
