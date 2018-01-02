c854a78aa69678a0974cf3969b5c24a77283f3d2
CarlosVRL <carlos.varela.eng@gmail.com>
2018-01-01 19:33:13 -0600

    Initial commit for "Create a Team!"

---
+++ src/week-1/CreateATeam/CreateATeam.cpp
@@ -0,0 +1,29 @@
+/**
+ * Create a Team!
+ */
+#include <fstream>
+
+using namespace std;
+
+/**
+ * File Management
+ */
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+/**
+ * Constants
+ */
+
+/**
+ * Prototypes
+ */
+
+/**
+ * Main
+ */
+int main(void)
+{
+    fout << "output\n";
+    return EXIT_SUCCESS;
+}
