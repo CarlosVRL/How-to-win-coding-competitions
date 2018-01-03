ba9c0b298f440e953fb6fb9e03d517d2a4e22b23
CarlosVRL <carlos.varela.eng@gmail.com>
2018-01-02 19:26:06 -0600

    Initial commit for Put the Chairs the Right Way!

---
+++ src/week-1/PutTheChairsTheRightWay/PutTheChairsTheRightWay.cpp
@@ -0,0 +1,37 @@
+/**
+ * Put the Chairs the Right Way!
+ */
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+/**
+ * File Management
+ */
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+/**
+ * Constants
+ */
+
+/**
+ * Prototypes
+ */
+
+/**
+ * Main
+ */
+int main(void)
+{
+    fout << "out" << endl;
+    
+    return EXIT_SUCCESS;
+}
+
+/**
+ * Functions
+ */
