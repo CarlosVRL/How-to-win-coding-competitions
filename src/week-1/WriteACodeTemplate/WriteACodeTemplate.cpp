e00d7da3f6475e5e4db86abc18d6bdc382dd398f
CarlosVRL <carlos.varela.eng@gmail.com>
2018-01-03 18:28:10 -0600

    Initial commit for WriteACodeTemplate

---
+++ src/week-1/WriteACodeTemplate/WriteACodeTemplate.cpp
@@ -0,0 +1,25 @@
+/**
+ * WriteACodeTemplate!
+ */
+#include <fstream>
+
+using namespace std;
+
+/**
+ * File Management
+ */
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+/**
+ * Constants
+ */
+
+/**
+ * Main
+ */
+int main(void)
+{   
+    fout << "out\n";
+    return EXIT_SUCCESS;
+}
